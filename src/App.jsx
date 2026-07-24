64b5897b701458f1095a2642ab0a9bab8e8749d6
Hari-Thopte <harshikathopte@gmail.com>
2026-07-25 00:30:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,18 @@ import Vessel from './pages/Vessel';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Simulation from './pages/Simulation';
-import OceanBackground from './components/Ocean/OceanBackground';
 import './App.css';
 
 function App() {
   return (
-    <div className="app-container">
-      <OceanBackground />
-      <div className="page-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/expeditions" element={<Expeditions />} />
-          <Route path="/vessel" element={<Vessel />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/simulation" element={<Simulation />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/expeditions" element={<Expeditions />} />
+      <Route path="/vessel" element={<Vessel />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/simulation" element={<Simulation />} />
+    </Routes>
   );
 }
 
